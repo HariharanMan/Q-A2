7ca54efe1ae9d4093543fa5c9c4a900a2bbc7f93
CyberHawk030 <cyber.hawk030@gmail.com>
2025-01-23 11:50:25 +0530

    Updated Login

--- main/urls.py
+++ main/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
-    path('',views.home,name='home'),
+    path('',views.login,name='login'),
+    path('signup/',views.signup,name='signup'),
+    path('home/',views.home,name='home'),
     path('Questions/', views.Questions, name="Question"),
     path('tags/' , views.tags , name ='tags'),
 ]

2ee1e776ca01409e97400cbde83f10fb934aed00
CyberHawk030 <cyber.hawk030@gmail.com>
2025-01-23 13:12:34 +0530

    Updated AdminHome page

--- main/urls.py
+++ main/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('home/',views.home,name='home'),
     path('questions/', views.questions, name="questions"),
     path('tags/' , views.tags , name ='tags'),
-    path('adminhome/' , views.admin , name ='adminhome'),
+    path('adminhome/' , views.adminhome , name ='adminhome'),
+    path('upload/' , views.upload , name ='upload'),
 ]
